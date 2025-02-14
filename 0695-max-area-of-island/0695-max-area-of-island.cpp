5ee987e7295aa2540ca1aaba3235c2da55ae6dca
Archet Srivastava <archetsrivastav056@gmail.com>
2025-02-14 14:55:31 +0530

    Time: 6 ms (31.12%), Space: 29.5 MB (32.65%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,39 +1,45 @@
 class Solution {
 public:
-
-     vector<int> dir={-1,0,1,0,-1};
-     int dfs(vector<vector<int>>&grid, vector<vector<bool>>&visited, int i, int j, int&row, int&col){
-        int max_area=1;
-        for(int d=0;d<4;d++){
-            int nrow= i+dir[d];
-            int ncol= j+dir[d+1];
-            if(nrow>=0 && nrow<row && ncol>=0 && ncol<col &&
-             !visited[nrow][ncol] && grid[nrow][ncol]>0){
-                visited[nrow][ncol]=true;
-                max_area+=dfs(grid,visited,nrow,ncol,row,col);
-            }
-        }
-       return max_area;
-    }
-
-
     int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        queue<pair<int,int>>q;
+        int total_area=0;
+        int area=0;
+        int delrow[4]={-1,0,1,0};
+        int delcol[4]={0,-1,0,1};
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    q.push({i,j});
+                    vis[i][j]=1;
+                    area=0;
 
-        int row=grid.size();
-        int col=grid[0].size();
-        vector<vector<bool>>visited(row,vector<bool>(col,false));
-        int max_islands=0;
+                    while(!q.empty()){
+                        
+                        int row=q.front().first;
+                        int col=q.front().second;
+                        q.pop();
+                        area++;
 
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(grid[i][j]>0 && !visited[i][j]){
-                    visited[i][j]=true;
-                    max_islands= max(max_islands,dfs(grid,visited,i,j,row,col));
+                        for(int i=0;i<4;i++){
+                            int nrow=row+delrow[i];
+                            int ncol=col+delcol[i];
+
+                            if(nrow<n && nrow>=0 && ncol<m && ncol>=0 && 
+                            vis[nrow][ncol]!=1 && grid[nrow][ncol]==1){
+                                q.push({nrow,ncol});
+                                vis[nrow][ncol]=1;
+                            }
+                        }   
+                    }
+                     total_area=max(area,total_area);
                 }
             }
         }
-        return max_islands;
 
+        return total_area;
     }
-      
 };
