623643b7c92cfe311af71932a828607e9eb10e6e
Archet Srivastava <archetsrivastav056@gmail.com>
2025-02-16 15:09:00 +0530

    Time: 3 ms (30.69%), Space: 17.2 MB (18.99%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,49 +3,46 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-
+        vector<vector<int>>vis(n,vector<int>(m,0));
         queue<pair<pair<int,int>,int>>q;
-        int vis[n][m];
-        int countFresh=0;
+        int delrow[4]={-1,0,1,0};
+        int delcol[4]={0,-1,0,1};
+        int freshcount=0;
+        int rottencount=0;
+        int time=0;
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
                     q.push({{i,j},0});
                     vis[i][j]=2;
-                }else{
-                    vis[i][j]=0;
                 }
-                if(grid[i][j]==1) countFresh++;
-            }
-        }
-
-        int delrow[4]={-1,0,1,0};
-        int delcol[4]={0,1,0,-1};
-        int tm=0;
-        int count=0;
-
-        while(!q.empty()){
-            int row=q.front().first.first;
-            int col=q.front().first.second;
-            int t=q.front().second;
-            q.pop();
-
-            for(int i=0;i<4;i++){
-                int nrow=row+delrow[i];
-                int ncol=col+delcol[i];
-                tm=max(tm,t);
-
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m 
-                && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
-                    q.push({{nrow,ncol},t+1});
-                    vis[nrow][ncol]=2;
-                    count++;
+                else if(grid[i][j]==1){
+                    freshcount++;
                 }
             }
-        }
+        }    
+               
+                while(!q.empty()){
+                    int row=q.front().first.first;
+                    int col=q.front().first.second;
+                    time=q.front().second;
+                    q.pop();
 
-        if(count!=countFresh) return -1;
-        return tm;
+                    for(int i=0;i<4;i++){
+                        int nrow=row+delrow[i];
+                        int ncol=col+delcol[i];
+
+                        if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&
+                         vis[nrow][ncol]!=2 && grid[nrow][ncol]==1){
+                            q.push({{nrow,ncol},time+1});
+                            vis[nrow][ncol]=2;
+                            rottencount++;
+                         }
+                    }
+                }
+                if(rottencount!=freshcount) return -1;
+
+        return time;
     }
 };
