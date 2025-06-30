26c8b52be65f9dc14edff92877294bc2da81aa5f
Archet Srivastava <archetsrivastav056@gmail.com>
2025-07-01 00:33:24 +0530

    Time: 0 ms (100%), Space: 19.6 MB (91.34%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -6,39 +6,36 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if (root == NULL)
-            return 0;
-        queue<pair<TreeNode*, int>> q;
-        q.push({root, 0});
-        int ans = 0;
+        queue<pair<TreeNode*,int>>q;
+        int ans=0;
+        if(root==NULL)return ans;
+        q.push({root,0});
+        
 
-        while (!q.empty()) {
-            int size = q.size();
-            int mmin=q.front().second;
-            int first, last;
-            for (int i = 0; i < size; i++) {
-                auto p = q.front();
+        while(!q.empty()){
+            int size=q.size();
+            int mini=q.front().second;
+            int first,last;
+
+            for(int i=0;i<size;i++){
+                auto p=q.front();
+                TreeNode*node=p.first;
+                long ind=p.second;
                 q.pop();
-                TreeNode* node = p.first;
-                long ind = p.second-mmin;
-                if (i == 0)
-                    first = ind;
-                if (i == size - 1)
-                    last = ind;
+                ind=ind-mini;
+                if(i==0) first=ind;
+                if(i==size-1) last=ind;
 
-                if (node->left)
-                    q.push({node->left, 2 * ind + 1});
-                if (node->right)
-                    q.push({node->right, 2 * ind + 2});
+                if(node->left) q.push({node->left,2*ind+1});
+                if(node->right) q.push({node->right,2*ind+2});
             }
-            ans = max(ans, last - first + 1);
+            ans=max(ans,last-first+1);
         }
         return ans;
     }
