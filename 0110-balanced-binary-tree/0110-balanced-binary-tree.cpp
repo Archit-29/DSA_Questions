965c6cea4d0b30dfa8a6674e7805e700b5a63647
Archet Srivastava <archetsrivastav056@gmail.com>
2025-07-01 18:24:34 +0530

    Time: 0 ms (100%), Space: 22.9 MB (97.55%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -6,33 +6,28 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    bool isBalanced(TreeNode* root) {
-        if (root == NULL)
-            return true;
-        int height = 1;
-        bst(root, height);
-        if (height == 1)
-            return true;
-        else
-            return false;
+
+    int helper(TreeNode*node,int&ans){
+        if(node==NULL) return 0;
+
+        int lh=helper(node->left,ans);
+        int rh=helper(node->right,ans);
+
+        if(abs(lh-rh)>1){
+            ans=0;
+        }
+        return 1+max(lh,rh);
     }
 
-    int bst(TreeNode* node, int& height) {
-
-        if (node == NULL)
-            return 0;
-
-        int lh = bst(node->left, height);
-        int rh = bst(node->right, height);
-
-        if (abs(lh - rh) > 1)
-            height = 0;
-        return 1 + max(lh, rh);
+    bool isBalanced(TreeNode* root) {
+        int ans=1;
+        helper(root,ans);
+        if(ans==0) return false;
+        return true;
     }
 };
