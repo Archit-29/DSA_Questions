b7884692671cbc8f18dd1b4cbd3e4b57cfafc166
Archet Srivastava <archetsrivastav056@gmail.com>
2025-07-11 01:04:39 +0530

    Time: 0 ms (100%), Space: 37.3 MB (43.61%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -9,19 +9,47 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class BSTiterator{
+    stack<TreeNode*> st;
+    bool isreverse;
+    public:
+        BSTiterator(TreeNode*root,bool reverse){
+            isreverse=reverse;
+            pushAll(root);
+        }
+
+        void pushAll(TreeNode*node){
+            while(node){
+                st.push(node);
+                node = (isreverse) ? node->right : node->left;
+            }
+        }
+
+        int next(){
+            TreeNode*node=st.top();
+            st.pop();
+            if(isreverse) pushAll(node->left);
+            else pushAll(node->right);
+            return node->val;
+        }
+    };
+
 class Solution {
 public:
-
-    bool helper(TreeNode*node,unordered_set<int>&seen,int target){
-        if(node==NULL) return false;
-        if(seen.count(target-node->val)) return true;
-        seen.insert(node->val);
-
-        return helper(node->left,seen,target) || helper(node->right,seen,target);
-    }
-
     bool findTarget(TreeNode* root, int k) {
-        unordered_set<int>seen;
-        return helper(root,seen,k);
+        if(root==NULL) return false;
+        BSTiterator l(root,false);
+        BSTiterator r(root,true);
+        int i=l.next();
+        int j=r.next();
+
+        while(i<j){
+            if(i+j==k) return true;
+            else if(i+j<k) i=l.next();
+            else{
+                j=r.next();
+            }
+        }
+        return false;
     }
 };
