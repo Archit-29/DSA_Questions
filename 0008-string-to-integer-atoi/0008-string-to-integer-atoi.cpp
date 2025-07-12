45936177e2c3a86ccbf3e6d509da6d80fa4ae022
Archet Srivastava <archetsrivastav056@gmail.com>
2025-07-12 20:22:27 +0530

    Time: 4 ms (6.77%), Space: 9.1 MB (56.49%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -3,27 +3,24 @@ public:
     int myAtoi(string s) {
         int n=s.size();
         int i=0;
-        long long ans=0;;
-        int sign=1;;
 
         while(i<n && s[i]==' '){
             i++;
         }
 
-        if(i<n && (s[i]=='-'||s[i]=='+')){
-            sign=(s[i]=='-')?-1:1;
+        int sign=1;
+        if(s[i]=='-' || s[i]=='+'){
+            sign= s[i]=='-' ? -1 : 1;
             i++;
         }
-
+        long long ans=0;;
         while(i<n && s[i]>='0' && s[i]<='9'){
-            int d= s[i]-'0';
-            ans=ans*10+d;
-
-            if(sign*ans<=INT_MIN) return INT_MIN;
-            if(sign*ans>=INT_MAX) return INT_MAX;
-
+            ans = ans*10 + (s[i]-'0');
             i++;
+            if(sign*ans <= INT_MIN) return INT_MIN;
+            if(sign*ans >= INT_MAX) return INT_MAX;
         }
+
         return sign*ans;
     }
 };
