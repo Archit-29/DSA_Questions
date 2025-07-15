b0815cad4c037488d7e61d8dbb7fe5d9c79f2657
Archet Srivastava <archetsrivastav056@gmail.com>
2025-07-15 18:37:45 +0530

    Time: 3 ms (13.85%), Space: 18.4 MB (36.57%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n=image.size();
-        int m=image[0].size();
+        int m=image.size();
+        int n=image[0].size();
+
+        int startcolor=image[sr][sc];
+        if (startcolor == color) return image;
+
         queue<pair<int,int>>q;
-
-        int startColor = image[sr][sc];
-        if(color==startColor) return image;
-
         q.push({sr,sc});
         image[sr][sc]=color;
 
+
         int delrow[4]={-1,0,1,0};
         int delcol[4]={0,1,0,-1};
+        
 
         while(!q.empty()){
             int row=q.front().first;
@@ -23,12 +25,11 @@ public:
                 int nrow=row+delrow[i];
                 int ncol=col+delcol[i];
 
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==startColor){
-                        q.push({nrow,ncol});
-                        image[nrow][ncol]=color;
-                    }
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && image[nrow][ncol]==startcolor){
+                    image[nrow][ncol]=color;
+                    q.push({nrow,ncol});
+                }
             }
-            
         }
         return image;
     }
