a8e6f80e55074716c939ab5f571ae17abd9aeabe
Archet Srivastava <archetsrivastav056@gmail.com>
2025-08-14 17:09:56 +0530

    Time: 0 ms (100%), Space: 9.1 MB (69.67%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,13 @@
 class Solution {
 private:
-    int helper(int row, int col, int m, int n,vector<vector<int>>&dp){
-        if(row<0 || col<0 || row>=m || col>=n) return 0;
-
+    int helper(int row, int col, int m, int n, vector<vector<int>>&dp){
+        if(row<0 || row>=m || col<0 || col>=n) return 0;
         if(row==m-1 && col==n-1) return 1;
 
         if(dp[row][col]!=-1) return dp[row][col];
 
-        dp[row][col]= helper(row+1,col,m,n,dp)+ helper(row,col+1,m,n,dp);
-
+        dp[row][col]= helper(row+1,col,m,n,dp)+helper(row,col+1,m,n,dp);
+        
         return dp[row][col];
     }
 public:
