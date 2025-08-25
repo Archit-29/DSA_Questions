593cbc8c8bd7bd27bf04aeff4b899b4d9756c555
Archet Srivastava <archetsrivastav056@gmail.com>
2025-08-25 21:19:49 +0530

    Time: 7 ms (65.62%), Space: 71.5 MB (44.61%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        unordered_map<int,int>mp;
-        int r=0;
-        int count=0;
-        int sum=0;
+        return atMost(nums, k) - atMost(nums, k - 1);
+    }
 
-        while(r<nums.size()){
-            if(nums[r]%2==1){
+    int atMost(vector<int>& nums, int k) {
+        if (k < 0)
+            return 0;
+        int r = 0;
+        int l = 0;
+        int sum = 0;
+        int count=0;
+
+        while (r < nums.size()) {
+
+            if (nums[r] % 2 == 1) {
                 sum++;
             }
-            if(sum==k) count++;
-            
-            int rem=sum-k;
-            if(mp.find(rem)!=mp.end()){
-                count+=mp[rem];
+            while (sum > k) {
+                if (nums[l] % 2 == 1) {
+                    sum--;
+                }
+                l++;
             }
-            mp[sum]++;
+            count+=r-l+1;
             r++;
         }
         return count;
