90c4c07a02e31930bff0a008620a159395629cf5
Archet Srivastava <archetsrivastav056@gmail.com>
2025-08-28 16:07:46 +0530

    Time: 2 ms (88.97%), Space: 9.9 MB (69.22%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     string countAndSay(int n) {
-       if(n==1) return "1";
-       if(n==2) return "11";
-       string str="11";
+        if(n==1) return "1";
+        if(n==2) return "11";
+        string str="11";
 
-       for(int i=3;i<=n;i++){
+        for(int i=3;i<=n;i++){
             string temp="";
-            str=str+"&";
+            str+='&';
             int count=1;
             for(int j=1;j<str.size();j++){
-                if(str[j]!=str[j-1]){
+                if(str[j]==str[j-1]){
+                    count++;
+                }else{
                     temp+=to_string(count);
                     temp+=str[j-1];
                     count=1;
-                }else{
-                    count++;
                 }
             }
             str=temp;
-       }
-       return str;
+        }
+        return str;
     }
 };
