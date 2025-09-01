3fe6ecbf198075ecb2b222864aaf3bfd1065f484
Archet Srivastava <archetsrivastav056@gmail.com>
2025-09-01 14:21:41 +0530

    Time: 51 ms (25.65%), Space: 49.2 MB (34.22%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    int f(int index, int amount, vector<int>&coins,vector<vector<int>>&dp){
+    int f(int index, int amount,vector<vector<int>>&dp, vector<int>&coins){
 
         if(index==0){
-            if(amount % coins[0]==0) return amount/coins[0];
-            else return 1e9;
+            if(amount%coins[0]==0) return amount/coins[0];
+            else{
+                return 1e9;
+            }
         }
 
         if(dp[index][amount]!=-1) return dp[index][amount];
-
-        int notTake=0+f(index-1,amount,coins,dp);
-
-        int take=1e9;
+        
+        int notPick=f(index-1,amount,dp,coins);
+        int pick=1e9;
         if(coins[index]<=amount){
-            take=1+f(index,amount-coins[index],coins,dp);
+            pick=1+f(index,amount-coins[index],dp,coins);
         }
-
-        return dp[index][amount]=min(notTake,take);
+        return dp[index][amount]=min(pick,notPick);
     }
     int coinChange(vector<int>& coins, int amount) {
-        int ans;
-
         int n=coins.size();
+        int ans=1e9;
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        ans= f(n-1,amount,coins,dp);
-
+        ans=f(n-1,amount,dp,coins);
         if(ans==1e9) return -1;
         else return ans;
     }
