01a9f0420ad7d8dd67cf9d353e56f67c1fc111b2
Archet Srivastava <archetsrivastav056@gmail.com>
2025-09-09 23:18:40 +0530

    Time: 39 ms (51.47%), Space: 49.3 MB (34.17%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    int f(int index, int amount, vector<vector<int>>&dp,vector<int>&coins){
 
-        if (amount == 0) return 0;
-        
+    int f(int index, int target, vector<int>&coins, vector<vector<int>>&dp){
+
         if(index==0){
-            if(amount%coins[index]==0) return amount/coins[index];
-            else return 1e9;
-        }
+            if(target % coins[0]==0) return target/coins[0];
+            return 1e9;
+        } 
 
-        if(dp[index][amount]!=-1) return dp[index][amount];
-
-        int notPick=0+f(index-1,amount,dp,coins);
-        int pick=1e9;
-        if(coins[index]<=amount){
-            pick= 1+f(index,amount-coins[index],dp,coins);
+        if(dp[index][target]!=-1) return dp[index][target];
+        
+        int notPick= 0+f(index-1,target,coins,dp);
+        int Pick=1e9;
+        if(coins[index]<=target){
+            Pick = 1+f(index,target-coins[index],coins,dp);
         }
-        return dp[index][amount]=min(pick,notPick);
+        return dp[index][target]=min(Pick,notPick);
     }
 
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans=f(n-1,amount,dp,coins);
-        return (ans==1e9) ? -1 : ans;
+        int ans= f(n-1,amount,coins,dp);
+        return (ans==1e9)?-1 : ans;
     }
 };
