b18942c6ade0b38835dbe576b07f52cf7e40e7ab
Archet Srivastava <archetsrivastav056@gmail.com>
2026-09-17 22:07:15 +0530

    Time: 43 ms (55.15%), Space: 19.1 MB (60.74%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,16 +4,17 @@ public:
         unordered_map<char,int>mp;
         int l=0;
         int r=0;
-        int length=0;
+        int ans=0;
 
         while(r<s.size()){
-            if(mp.find(s[r])!=mp.end()){
-                l=max(l,mp[s[r]]+1);            
+            if(mp.find(s[r])!=mp.end() && mp[s[r]]>=l){
+                l=mp[s[r]]+1;
             }
             mp[s[r]]=r;
-            length=max(length,r-l+1);
+            ans=max(ans,r-l+1);
             r++;
+            
         }
-        return length;
+        return ans;
     }
 };
